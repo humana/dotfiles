00c6f9aaef4389cd6694c915c6aa3002448c0534
humana <aniello.human@gmail.com>
2012-10-25 16:39:54 +0200

    added gtk fix for f10

--- install.sh
+++ install.sh
@@ -10,18 +10,28 @@ if [ ! -d bak ]; then
 	mkdir bak
 fi
 mv ~/.vim bak/
-mv ~/.vimrc bak/
+mv ~/.vimrc bak/ 
 mv ~/.bashrc bak/
 mv ~/.bash_profile bak/
+mv ~/.gitconfig bak/
 
 # vim install
 if [ ! -d ~/.vim ]; then
 	mkdir ~/.vim
 fi
-mv bundle ~/.vim
-ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s bundle ~/.vim/bundle
+ln -s vimrc ~/.vimrc
 vim +BundleInstall +qall
 
+# git
+ln -s gitconfig ~/.gitconfig
+
 # bash
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/bashrc_profile ~/.bash_profile
+ln -s bashrc ~/.bashrc
+ln -s bash_profile ~/.bash_profile
+
+# gtk
+if [ ! -d ~/.config/gtk-3.0 ]
+	mkdir -p ~/.config/gtk-3.0
+fi
+cp misc/gtk.css ~/.config/gtk-3.0/
